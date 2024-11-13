f12ecfd73d54b669e97250abdeaf85d1bb907723
Wei Zhe Wong <93571744+Kurosume@users.noreply.github.com>
2024-11-13 16:00:41 +0800

    update to lesson_plan.py

--- lesson_plan.py
+++ lesson_plan.py
@@ -1,4 +1,5 @@
-
+import streamlit as st
+import wikipi
 import os
 from whapi import get_id
 from whapi import random_article
@@ -9,53 +10,53 @@ from whapi import search
 from whapi import return_details
 
 
-load_dotenv()
-
 client = OpenAI(api_key=os.environ.get('OPENAI_API_KEY')) 
 
+st.title("Plan your lesson 101")
 
+goal = st.text_input("Type your goal that you want to achieve")
+if st.button("Generate"):
+    def generate_goal():
 
+        search_results = search(goal, 1)
 
-x = input("Tell me your goal that you want to achieve: ")
+        URL = search_results[0:2][0]['url']
 
-search_results = search(x, 1)
-#article_info = return_details(search_results[0:3])
+        response_hobby= client.chat.completions.create(
+            model="gpt-4o-mini",
 
-URL = search_results[0:2][0]['url']
-
-response_hobby= client.chat.completions.create(
-    model="gpt-4o-mini",
-
-    messages=[
+            messages=[
+                
+                {"role": "system", "content": """ 
+                You are given the task to design a lesson plan for the user based on""" 
+                + goal +
+                """
+                The output should be in json without any additionally formatting and describe the tasks.
+                    {
+                        "Prerequisite": str
+                        "Month Number":int
+                        "Week number":{
+                            "Task number": str
+                            "Task number": str
+                            }
+                        "Month Number":int
+                        "Week number":{
+                            "Task number": str
+                            "Task number": str
+                            }
+                        "Month Number":int
+                        "Week number":{
+                            "Task number": str
+                            "Task number": str
+                            }
+                    }
+                The month number, week number and tasks can be more than two.
+                """},
+                {"role": "user", "content": "Please summarize and give a training plan based on " + URL},
+            ]
         
-        {"role": "system", "content": """ 
-         You are given the task to design a lesson plan for the user based on""" 
-         + x +
-         """
-         The output should be in json without any additionally formatting and describe the tasks.
-            {
-                "Prerequisite": str
-                "Month Number":int
-                "Week number":{
-                    "Task number": str
-                    "Task number": str
-                    }
-                "Month Number":int
-                "Week number":{
-                    "Task number": str
-                    "Task number": str
-                    }
-                "Month Number":int
-                "Week number":{
-                    "Task number": str
-                    "Task number": str
-                    }
-            }
-        The task can be more than two.
-        """},
-        {"role": "user", "content": "Please summarize and give a training plan based on " + URL},
-    ]
-)
-
-print(URL)
-print(response_hobby.choices[0].message.content)
+        )
+        return st.write(response_hobby.choices[0].message.content)
+    generate_goal()
+else:
+    st.balloons()
