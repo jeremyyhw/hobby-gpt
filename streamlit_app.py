9001a63e5f6ff42b21c7f2c112860a90b1874934
jeremyyhw <jeremy.yhw@gmail.com>
2024-11-13 12:02:24 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,3 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
-
-st.secrets["OPENAI_API_KEY"]
