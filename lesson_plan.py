a5aadf9377ef219ba15ef05adc41d4d27403b7ba
Wei Zhe Wong <93571744+Kurosume@users.noreply.github.com>
2024-11-13 16:21:30 +0800

    Update lesson_plan.py

--- lesson_plan.py
+++ lesson_plan.py
@@ -15,48 +15,52 @@ client = OpenAI(api_key=os.environ.get('OPENAI_API_KEY'))
 st.title("Plan your lesson 101")
 
 goal = st.text_input("Type your goal that you want to achieve")
-if st.button("Generate"):
-    def generate_goal():
+def generate_goal():
 
-        search_results = search(goal, 1)
+    search_results = search(goal, 1)
 
-        URL = search_results[0:2][0]['url']
+    URL = search_results[0:2][0]['url']
 
-        response_hobby= client.chat.completions.create(
-            model="gpt-4o-mini",
+    response_hobby= client.chat.completions.create(
+        model="gpt-4o-mini",
 
-            messages=[
-                
-                {"role": "system", "content": """ 
-                You are given the task to design a lesson plan for the user based on""" 
-                + goal +
-                """
-                The output should be in json without any additionally formatting and describe the tasks.
-                    {
-                        "Prerequisite": str
-                        "Month Number":int
-                        "Week number":{
-                            "Task number": str
-                            "Task number": str
-                            }
-                        "Month Number":int
-                        "Week number":{
-                            "Task number": str
-                            "Task number": str
-                            }
-                        "Month Number":int
-                        "Week number":{
-                            "Task number": str
-                            "Task number": str
-                            }
-                    }
-                The month number, week number and tasks can be more than two.
-                """},
-                {"role": "user", "content": "Please summarize and give a training plan based on " + URL},
-            ]
-        
-        )
-        return st.write(response_hobby.choices[0].message.content)
-    generate_goal()
-else:
-    st.balloons()
+        messages=[
+            
+            {"role": "system", "content": """ 
+            You are given the task to design a lesson plan for the user based on""" 
+            + goal +
+            """
+            The output should be in json without any additionally formatting and describe the tasks.
+                {
+                    "Prerequisite": str
+                    "Month Number":int
+                    "Week number":{
+                        "Task number": str
+                        "Task number": str
+                        }
+                    "Month Number":int
+                    "Week number":{
+                        "Task number": str
+                        "Task number": str
+                        }
+                    "Month Number":int
+                    "Week number":{
+                        "Task number": str
+                        "Task number": str
+                        }
+                }
+            The month number, week number and tasks can be more than two.
+            """},
+            {"role": "user", "content": "Please summarize and give a training plan based on " + URL},
+        ]
+    
+    )
+    return st.write(response_hobby.choices[0].message.content)
+
+if st.button("Generate Plan"):
+    if goal == "":
+        st.write("What? You have no goals in life?")
+    else:
+        generate_goal()
+
+    
